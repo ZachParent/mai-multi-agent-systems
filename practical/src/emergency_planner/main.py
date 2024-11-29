8a8ee76189812ff51cfbff0f121185170a22f3c4
Zach Parent <zachparent@duck.com>
2024-11-29 15:11:23 +0100

    create flow

--- practical/src/emergency_planner/main.py
+++ practical/src/emergency_planner/main.py
@@ -5,55 +5,54 @@ from pydantic import BaseModel
 
 from crewai.flow.flow import Flow, listen, start
 
-from .crews.public_communication.public_communication import PublicCommunicationCrew
 from .crews.emergency_services.emergency_services import EmergencyServicesCrew
+from .crews.fire_fighters.fire_fighters import FireFightersCrew
+from .data_models import EmergencyPlannerState, EmergencyCall
 
-
-class PoemState(BaseModel):
-    sentence_count: int = 1
-    poem: str = ""
-
-
-class PoemFlow(Flow[PoemState]):
+class EmergencyPlannerFlow(Flow[EmergencyPlannerState]):
+    def _create_initial_state(self) -> EmergencyPlannerState:
+        return EmergencyPlannerState(
+            emergency_call=None,
+            fire_fighter_dispatch=None
+        )
 
     @start()
-    def generate_sentence_count(self):
-        print("Generating sentence count")
-        self.state.sentence_count = randint(1, 5)
+    def receive_call(self):
+        print("Receiving call")
+        self.state.emergency_call = EmergencyCall(location="123 Main St", description="Fire", timestamp="2021-01-01 12:00:00")
+        result = EmergencyServicesCrew().crew().kickoff(inputs={"emergency_call": self.state.emergency_call})
+        print("Emergency call received", result.raw)
+        self.state.fire_fighter_dispatch = result.raw
+
+    @listen(receive_call)
+    def dispatch_fire_fighters(self):
+        print("Dispatching fire fighters")
+        result = EmergencyServicesCrew().crew().kickoff(inputs={"emergency_call": self.state.emergency_call})
+        print("Fire fighters dispatched", result.raw)
+        self.state.fire_fighter_dispatch = result.raw
     
-    @listen(generate_sentence_count)
-    def generate_poem(self):
-        print("Generating poem")
-        result = "Hello from the generate_poem method"
-        self.state.poem = result
+    @listen(dispatch_fire_fighters)
+    def plan_route(self):
+        print("Planning route")
+        result = FireFightersCrew().crew().kickoff(inputs={"fire_fighter_dispatch": self.state.fire_fighter_dispatch})
+        print("Route planned", result.raw)
+        self.state.fire_fighter_dispatch = result.raw
 
-    # @listen(generate_sentence_count)
-    # def generate_poem(self):
-    #     print("Generating poem")
-    #     result = (
-    #         PublicCommunicationsCrew()
-    #         .crew()
-    #         .kickoff(inputs={"sentence_count": self.state.sentence_count})
-    #     )
-
-    #     print("Poem generated", result.raw)
-    #     self.state.poem = result.raw
-
-    @listen(generate_poem)
-    def save_poem(self):
-        print("Saving poem")
-        with open("poem.txt", "w") as f:
-            f.write(self.state.poem)
+    @listen(plan_route)
+    def save_route(self):
+        print("Saving route")
+        with open("data/outputs/route.txt", "w") as f:
+            f.write(self.state.fire_fighter_dispatch)
 
 
 def kickoff():
-    poem_flow = PoemFlow()
-    poem_flow.kickoff()
+    emergency_planner_flow = EmergencyPlannerFlow()
+    emergency_planner_flow.kickoff()
 
 
 def plot():
-    poem_flow = PoemFlow()
-    poem_flow.plot()
+    emergency_planner_flow = EmergencyPlannerFlow()
+    emergency_planner_flow.plot()
 
 
 if __name__ == "__main__":
