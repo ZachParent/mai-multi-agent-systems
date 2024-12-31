8e74896584e89508f27b352e4cf232a608862ff0
brunosangom <brunosangom@gmail.com>
2024-12-31 13:55:30 +0100

    Medical services crew configuration and tasks

--- practical/src/emergency_planner/crews/medical_services/medical_services.py
+++ practical/src/emergency_planner/crews/medical_services/medical_services.py
@@ -2,20 +2,43 @@ from crewai import Agent, Task, Crew, Process
 from crewai.project import CrewBase, agent, task, crew
 from data_models import MedicalAssessment
 
-
 @CrewBase
 class MedicalServicesCrew:
     """Medical Services Crew"""
 
     @agent
-    def fire_fighter(self) -> Agent:
-        return Agent(config=self.agents_config["fire_fighter"])
+    def medical_services_operator(self) -> Agent:
+        return Agent(config=self.agents_config["medical_services_operator"])
+
+    @agent
+    def hospital_coordinator(self) -> Agent:
+        return Agent(config=self.agents_config["hospital_coordinator"])
+
+    @agent
+    def paramedics(self) -> Agent:
+        return Agent(config=self.agents_config["paramedics"])
 
     @task
-    def receive_dispatch(self, input_pydantic: MedicalAssessment) -> Task:
-        return Task(config=self.tasks_config["receive_dispatch"])
+    def receive_report(self) -> Task:
+        return Task(config=self.tasks_config["receive_report"])
+
+    @task
+    def rank_hospitals(self) -> Task:
+        return Task(config=self.tasks_config["rank_hospitals"])
+
+    @task
+    def allocate_hospital_resources(self) -> Task:
+        return Task(config=self.tasks_config["allocate_hospital_resources"])
+
+    @task
+    def deploy_paramedics(self) -> Task:
+        return Task(config=self.tasks_config["deploy_paramedics"])
+
+    @task
+    def report_medical_response(self) -> Task:
+        return Task(config=self.tasks_config["report_medical_response"])
 
     @crew
     def crew(self) -> Crew:
-        """Creates the Public Communication Crew"""
+        """Creates the Medical Services Crew"""
         return Crew(agents=self.agents, tasks=self.tasks, process=Process.sequential)
