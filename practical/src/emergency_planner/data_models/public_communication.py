969afecad3cd6e18442bf13aca5daec9837ec0bd
Antonio Lobo Desarrollo <alobosantos10@gmail.com>
2024-12-31 14:03:19 +0100

    (fix) missing import

--- practical/src/emergency_planner/data_models/public_communication.py
+++ practical/src/emergency_planner/data_models/public_communication.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import List
 from datetime import datetime
 
-from .shared import FireSeverity
+from .shared import FireSeverity, FireType
 from .emergency_services import CallAssessment
 from .firefighters import FirefightersResponseReport
 from .medical_services import MedicalResponseReport
