06e5f25c1795c6509c841681f7457ec3d3bbdd73
Zach Parent <zachparent@duck.com>
2025-01-09 17:29:25 +0100

    start from checkpoint for PC team

--- practical/src/emergency_planner/main.py
+++ practical/src/emergency_planner/main.py
@@ -12,6 +12,7 @@ from .data_models import (
     MedicalAssessment,
     EmergencyReport,
 )
+from pydantic_core import from_json
 
 logger = logging.getLogger(__name__)
 # logging.basicConfig(level=logging.INFO)
@@ -32,69 +33,73 @@ class EmergencyPlannerFlow(Flow[EmergencyPlannerState]):
             public_communication_report=None,
         )
 
+    # @start()
+    # def get_call_transcript(self):
+    #     logger.info("Getting call transcript")
+    #     with open(EMERGENCY_CALL_TRANSCRIPTS_FILENAME, "r") as f:
+    #         self.state.call_transcript = f.readlines()[TRANSCRIPT_INDEX]
+    #     logger.info("Call transcript received", self.state.call_transcript)
+
+    # @listen(get_call_transcript)
+    # def emergency_services(self):
+    #     logger.info("Receiving call")
+    #     result = (
+    #         EmergencyServicesCrew()
+    #         .crew()
+    #         .kickoff(inputs={"transcript": self.state.call_transcript})
+    #     )
+    #     self.state.call_assessment = json.loads(result.raw)
+    #     logger.info("Emergency call received", result.raw)
+
+    # @listen(emergency_services)
+    # def firefighters(self):
+    #     logger.info("Dispatching fire fighters")
+    #     fire_assessment = FireAssessment(**self.state.call_assessment)
+    #     result = (
+    #         FirefightersCrew()
+    #         .crew()
+    #         .kickoff(inputs={"fire_assessment": fire_assessment.model_dump_json()})
+    #     )
+    #     self.state.firefighters_response_report = json.loads(result.raw)
+    #     logger.info("Fire fighters dispatched", result.raw)
+
+    # @listen(emergency_services)
+    # def medical_services(self):
+    #     if not self.state.call_assessment['medical_services_required']:
+    #         logger.info("Medical services not required")
+    #         return
+    #     logger.info("Dispatching medical services")
+    #     self.state.call_assessment['injured_count'] = len(self.state.call_assessment['injured_details'])
+    #     medical_assessment = MedicalAssessment(**self.state.call_assessment)
+    #     result = (
+    #         MedicalServicesCrew()
+    #         .crew()
+    #         .kickoff(inputs={"medical_assessment": medical_assessment.model_dump_json()})
+    #     )
+    #     self.state.medical_response_report = json.loads(result.raw)
+    #     logger.info("Medical services dispatched", result.raw)
+
+    # @listen(or_(and_(firefighters, medical_services), "retry_public_communication"))
+
+
+    EMERGENCY_REPORT = EmergencyReport.model_validate(from_json("""{"call_assessment":{"fire_type":"electrical","location":[41.71947,2.84031],"injured_details":["minor","severe"],"fire_severity":"high","hazards":["gas cylinders","flammable chemicals"],"indoor":true,"trapped_people":5,"firefighters_required":true,"medical_services_required":true},"firefighters_response_report":{"summary":"The firefighting response involved deploying 10 fire combatants to address an electrical high-severity fire at an indoor location. The estimated arrival time was 2023-09-22T14:30:00Z. Primary attack and ventilation activities were assigned with high and medium priority, respectively. There were 5 trapped people and hazards included gas cylinders and flammable chemicals.","timestamp":"2023-09-22T15:45:00Z"},"medical_response_report":{"summary":"The medical response plan is in place with Hospital1 and Hospital2 allocated for resource support. Paramedics are deployed to the emergency location with estimated arrival times of 2023-02-20T14:45:00Z and 2023-02-20T15:00:00Z, respectively. The equipment list includes oxygen masks, stretchers, defibrillators, and IV drips for both patients' stabilization and treatment.","timestamp":"2023-02-20T14:30:00Z"},"timestamp":"2023-09-22T15:45:00Z","fire_type":"electrical","fire_severity":"high","location_x":41.71947,"location_y":2.84031}"""))
     @start()
-    def get_call_transcript(self):
-        logger.info("Getting call transcript")
-        with open(EMERGENCY_CALL_TRANSCRIPTS_FILENAME, "r") as f:
-            self.state.call_transcript = f.readlines()[TRANSCRIPT_INDEX]
-        logger.info("Call transcript received", self.state.call_transcript)
-
-    @listen(get_call_transcript)
-    def emergency_services(self):
-        logger.info("Receiving call")
-        result = (
-            EmergencyServicesCrew()
-            .crew()
-            .kickoff(inputs={"transcript": self.state.call_transcript})
-        )
-        self.state.call_assessment = json.loads(result.raw)
-        logger.info("Emergency call received", result.raw)
-
-    @listen(emergency_services)
-    def firefighters(self):
-        logger.info("Dispatching fire fighters")
-        fire_assessment = FireAssessment(**self.state.call_assessment)
-        result = (
-            FirefightersCrew()
-            .crew()
-            .kickoff(inputs={"fire_assessment": fire_assessment.model_dump_json()})
-        )
-        self.state.firefighters_response_report = json.loads(result.raw)
-        logger.info("Fire fighters dispatched", result.raw)
-
-    @listen(emergency_services)
-    def medical_services(self):
-        if not self.state.call_assessment['medical_services_required']:
-            logger.info("Medical services not required")
-            return
-        logger.info("Dispatching medical services")
-        self.state.call_assessment['injured_count'] = len(self.state.call_assessment['injured_details'])
-        medical_assessment = MedicalAssessment(**self.state.call_assessment)
-        result = (
-            MedicalServicesCrew()
-            .crew()
-            .kickoff(inputs={"medical_assessment": medical_assessment.model_dump_json()})
-        )
-        self.state.medical_response_report = json.loads(result.raw)
-        logger.info("Medical services dispatched", result.raw)
-
-    @listen(or_(and_(firefighters, medical_services), "retry_public_communication"))
     def public_communication(self):
         logger.info("Handling public communication")
-        emergency_report = EmergencyReport(
-            call_assessment=self.state.call_assessment,
-            firefighters_response_report=self.state.firefighters_response_report,
-            medical_response_report=self.state.medical_response_report,
-            timestamp=self.state.firefighters_response_report['timestamp'],
-            fire_type=self.state.call_assessment['fire_type'],
-            fire_severity=self.state.call_assessment['fire_severity'],
-            location_x=self.state.call_assessment['location'][0],
-            location_y=self.state.call_assessment['location'][1]
-        )
+        # emergency_report = EmergencyReport(
+        #     call_assessment=self.state.call_assessment,
+        #     firefighters_response_report=self.state.firefighters_response_report,
+        #     medical_response_report=self.state.medical_response_report,
+        #     timestamp=self.state.firefighters_response_report['timestamp'],
+        #     fire_type=self.state.call_assessment['fire_type'],
+        #     fire_severity=self.state.call_assessment['fire_severity'],
+        #     location_x=self.state.call_assessment['location'][0],
+        #     location_y=self.state.call_assessment['location'][1]
+        # )
         result = (
             PublicCommunicationCrew()
             .crew()
-            .kickoff(inputs={"emergency_report": emergency_report.model_dump_json()})
+            .kickoff(inputs={"emergency_report": self.EMERGENCY_REPORT.model_dump_json()})
         )
         self.state.public_communication_report = json.loads(result.raw)
         logger.info("Public communication handled", result.raw)
